108baf0f6d4bc1ccb50307136d1fb38b32365858
Craig Christopher Messi <craig.messi@gmail.com>
2020-04-29 16:13:52 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do
+  get '/articles/new' do
+    erb :new
+  end
+
+  post '/articles' do
+    @post = Article.create(params)
+    redirect to '/articles'
+  end
+
+  get '/articles' do
+
+   @posts = Article.all
+
+   erb :index
+
+  end
+
+  get '/articles/:id' do
+
+    @post = Article.find_by_id(params[:id])
+    erb :show
+
+  end
+
+  get '/articles/:id/edit' do
+    @post = Article.find_by_id(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    @post = Article.find_by_id(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+  delete '/articles/:id/delete' do
+    @post = Article.find_by_id(params[:id])
+    @post.delete
+    erb :deleted
   end
 end
